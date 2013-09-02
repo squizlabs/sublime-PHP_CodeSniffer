38bc29d62d4b7049b6e7bae27a79c67d02c4f8f2
Sertan Danis <sdanis@squiz.com.au>
2013-09-02 15:09:44 +1000

    Refactored code. Clicking a line on diff view now jumps to that line on file view. Only the clickable lines are now highlighted

--- PHP_CodeSniffer.py
+++ PHP_CodeSniffer.py
@@ -10,9 +10,13 @@ RESULT_VIEW_NAME = 'phpcs_result_view'
 settings         = sublime.load_settings('PHP_CodeSniffer.sublime-settings')
 
 class PHP_CodeSniffer:
-  def runPhpcbf(self, window, content=''):
-    if not content:
-      content = window.active_view().substr(sublime.Region(0, window.active_view().size()))
+  # Type of the view, phpcs or phpcbf.
+  file_view = None
+  view_type = None
+  window    = None
+
+  def runPhpcbf(self, window):
+    content = window.active_view().substr(sublime.Region(0, window.active_view().size()))
 
     args = [settings.get('phpcbf_path', 'phpcbf')]
 
@@ -26,7 +30,35 @@ class PHP_CodeSniffer:
     if proc.stdout:
       newContent = proc.communicate(content)[0]
 
-    self.runDiff(window, content, newContent)
+    # Get the diff between content and the new content.
+    difftxt = self.runDiff(window, content, newContent)
+    if not difftxt:
+      self.clear_view()
+      return
+
+    # Remove the gutter markers.
+    window.active_view().erase_regions('errors')
+    window.active_view().erase_regions('warnings')
+    self.window = window
+    self.file_view = window.active_view()
+
+    # Show results panel.
+    outputView = self.initResultsPanel(window)
+    self.showResultsPanel(window)
+    self.view_type = 'phpcbf'
+
+    # Replace the main view contents with the fixed content.
+    mainEdit = window.active_view().begin_edit()
+    window.active_view().replace(mainEdit, sublime.Region(0, window.active_view().size()), newContent)
+    window.active_view().end_edit(mainEdit)
+
+    # Print the diff in output view.
+    outputView.set_read_only(False)
+    edit = outputView.begin_edit()
+    outputView.insert(edit, 0, difftxt)
+    outputView.end_edit(edit)
+    outputView.set_read_only(True)
+
 
   def runDiff(self, window, origContent, newContent):
     try:
@@ -36,36 +68,19 @@ class PHP_CodeSniffer:
         sublime.status_message("Diff only works with UTF-8 files")
         return
 
+    # Get the diff between original content and the fixed content.
     diff = difflib.unified_diff(a, b, 'Original', 'Fixed', lineterm='')
     difftxt = u"\n".join(line for line in diff)
 
     if difftxt == "":
-        sublime.status_message("No changes")
-        return
+      sublime.status_message('PHP_CodeSniffer did not make any changes')
+      return
 
     difftxt = "\n PHP_CodeSniffer made the following fixes to this file:\n\n" + difftxt
-
-    window.active_view().erase_regions('errors')
-    window.active_view().erase_regions('warnings')
-
-    mainEdit = window.active_view().begin_edit()
-    window.active_view().replace(mainEdit, sublime.Region(0, window.active_view().size()), newContent)
-    window.active_view().end_edit(mainEdit)
-
-    v = window.get_output_panel('unsaved_changes')
-    v.set_syntax_file('Packages/Diff/Diff.tmLanguage')
-    v.settings().set('word_wrap', window.active_view().settings().get('word_wrap'))
-
-    v.set_read_only(False)
-    edit = v.begin_edit()
-    v.insert(edit, 0, difftxt)
-    v.end_edit(edit)
-    v.set_read_only(True)
-
-    window.run_command("show_panel", {"panel": "output.unsaved_changes"})
+    return difftxt
 
 
-  def runPhpcs(self, window, outputView, content):
+  def runPhpcs(self, window):
     # PHPCS cmd args.
     args = [settings.get('phpcs_path', 'phpcs')]
 
@@ -78,11 +93,19 @@ class PHP_CodeSniffer:
     args.append('--report=' + sublime.packages_path() + '/PHP_CodeSniffer/STPluginReport.php')
 
     proc = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+    content = window.active_view().substr(sublime.Region(0, window.active_view().size()))
+
     if proc.stdout:
       data = proc.communicate(content)[0]
 
+    outputView = self.initResultsPanel(window)
+    self.showResultsPanel(window)
+    self.view_type = 'phpcs'
+    self.window = window
+    self.file_view = window.active_view()
+
     outputView.set_read_only(False)
-    outputView.set_syntax_file('Packages/Diff/Diff.tmLanguage')
     edit = outputView.begin_edit()
     outputView.insert(edit, outputView.size(), data)
     outputView.end_edit(edit)
@@ -94,6 +117,7 @@ class PHP_CodeSniffer:
     warn_regions = []
     col_regions  = []
     msg_type     = ''
+
     for line in lines:
       if line == ' Errors:':
         msg_type = 'error'
@@ -114,6 +138,85 @@ class PHP_CodeSniffer:
     window.active_view().add_regions('errors', err_regions, settings.get('error_scope'), '../PHP_CodeSniffer/icons/error', sublime.HIDDEN)
     window.active_view().add_regions('warnings', warn_regions, settings.get('warning_scope'), '../PHP_CodeSniffer/icons/warning', sublime.HIDDEN)
 
+
+  def initResultsPanel(self, window):
+    if not hasattr(self, 'output_view'):
+      self.output_view = window.get_output_panel(RESULT_VIEW_NAME)
+      self.output_view.set_syntax_file('Packages/Diff/Diff.tmLanguage')
+      self.output_view.set_name(RESULT_VIEW_NAME)
+
+    self.clear_view()
+    self.output_view.settings().set("file_path", window.active_view().file_name())
+    return self.output_view
+
+
+  def showResultsPanel(self, window):
+    window.run_command("show_panel", {"panel": "output."+RESULT_VIEW_NAME})
+
+
+  def clear_view(self):
+    self.output_view.set_read_only(False)
+    edit = self.output_view.begin_edit()
+    self.output_view.erase(edit, sublime.Region(0, self.output_view.size()))
+    self.output_view.end_edit(edit)
+    self.output_view.set_read_only(True)
+
+
+  def line_clicked(self):
+    if self.view_type == 'phpcs':
+        self.handle_phpcs_line_click()
+    else:
+        self.handle_phpcbf_line_click()
+
+
+  def handle_phpcs_line_click(self):
+    region = self.output_view.line(self.output_view.sel()[0])
+    line   = self.output_view.substr(region)
+
+    if line.find('[ Click here to fix this file ]') != -1:
+      phpcs.runPhpcbf(self.window)
+      return
+    else:
+      match = re.match(r'[^:0-9]+([0-9]+)\s*:', line)
+      if not match:
+        return
+
+    # Highlight the clicked results line.
+    self.output_view.add_regions(RESULT_VIEW_NAME, [region], "string", '', sublime.DRAW_OUTLINED)
+
+    lineNum = match.group(1)
+    self.go_to_line(lineNum)
+
+
+  def handle_phpcbf_line_click(self):
+    pnt    = self.output_view.sel()[0]
+    region = self.output_view.line(pnt)
+    line   = self.output_view.substr(region)
+    (row, col) = self.output_view.rowcol(pnt.begin())
+
+    offset = 0
+    found  = False
+    while not found and row > 0:
+      text_point = self.output_view.text_point(row, 0)
+      line = self.output_view.substr(self.output_view.line(text_point))
+      if line.startswith('@@'):
+        match = re.match(r'^@@ -\d+,\d+ \+(\d+),.*', line)
+        if match:
+          lineNum = int(match.group(1)) + offset - 1
+          self.go_to_line(lineNum)
+
+        return
+      elif not line.startswith('-'):
+        offset = offset + 1
+
+      row = row - 1
+
+
+  def go_to_line(self, lineNum):
+    self.window.focus_view(self.file_view)
+    self.file_view.run_command("goto_line", {"line": lineNum})
+
+
 # Init PHPCS.
 phpcs = PHP_CodeSniffer()
 
@@ -123,44 +226,11 @@ class PhpcbfCommand(sublime_plugin.WindowCommand):
 
 class PhpcsCommand(sublime_plugin.WindowCommand):
   def run(self):
-    file_path = self.window.active_view().file_name()
-    file_name = os.path.basename(file_path)
-
-    self.file_path = file_path
-    self.file_name = file_name
-    self.resultsPanelVisible = False
-
-    self.initResultsPanel()
-    self.showResultsPanel()
-
-    content = self.window.active_view().substr(sublime.Region(0, self.window.active_view().size()))
-    phpcs.runPhpcs(self.window, self.output_view, content)
-
-
-  def initResultsPanel(self):
-    if not hasattr(self, 'output_view'):
-      self.output_view = self.window.get_output_panel(RESULT_VIEW_NAME)
-      self.output_view.set_name(RESULT_VIEW_NAME)
-    self.clear_view()
-    self.output_view.settings().set("file_path", self.file_path)
-
-  def showResultsPanel(self):
-    if self.resultsPanelVisible:
-      return
-    self.window.run_command("show_panel", {"panel": "output."+RESULT_VIEW_NAME})
-    self.resultsPanelVisible = True
-
-  def clear_view(self):
-    self.output_view.set_read_only(False)
-    edit = self.output_view.begin_edit()
-    self.output_view.erase(edit, sublime.Region(0, self.output_view.size()))
-    self.output_view.end_edit(edit)
-    self.output_view.set_read_only(True)
+    phpcs.runPhpcs(self.window)
 
 class PhpcsEventListener(sublime_plugin.EventListener):
   def __init__(self):
     self.previous_region = None
-    self.file_view = None
 
   def on_query_context(self, view, key, operator, operand, match_all):
     # TODO: No idea if this is the right way but seems to work o.O
@@ -187,37 +257,4 @@ class PhpcsEventListener(sublime_plugin.EventListener):
       return
 
     self.previous_region = region
-    window = sublime.active_window()
-
-    line = view.substr(region)
-
-    if line.find('[ Click here to fix this file ]') != -1:
-      phpcs.runPhpcbf(window)
-      return
-
-    match = re.match(r'[^:0-9]+([0-9]+)\s*:', line)
-    if not match:
-      return
-
-    # Highlight the clicked results line.
-    view.add_regions(RESULT_VIEW_NAME, [region], "string", '', sublime.DRAW_OUTLINED)
-
-    # Find the file view.
-    file_view = self.file_view
-    if not file_view:
-      file_path = view.settings().get('file_path')
-      file_view = None
-      for v in window.views():
-        if v.file_name() == file_path:
-          file_view = v
-          break
-
-      if file_view == None:
-        return
-
-      self.file_view = file_view
-
-    lineNum = int(match.group(1))
-    window.focus_view(file_view)
-    file_view.run_command("goto_line", {"line": lineNum})
-    file_region = file_view.line(file_view.sel()[0])
+    phpcs.line_clicked()
