abd641d0209a57702829c8e81303f1f15a96f0f9
Sertan Danis <sdanis@squiz.com.au>
2013-09-03 15:06:02 +1000

    Added threading.

--- PHP_CodeSniffer.py
+++ PHP_CodeSniffer.py
@@ -5,6 +5,7 @@ import sublime_plugin
 import subprocess
 import StringIO
 import difflib
+import threading
 
 RESULT_VIEW_NAME = 'phpcs_result_view'
 settings         = sublime.load_settings('PHP_CodeSniffer.sublime-settings')
@@ -15,34 +16,12 @@ class PHP_CodeSniffer:
   view_type = None
   window    = None
 
-  def runPhpcbf(self, window):
+  def run(self, window, cmd):
     content = window.active_view().substr(sublime.Region(0, window.active_view().size()))
+    t = threading.Thread(target=self.run_command, args=(self.get_command_args(cmd), cmd, content, window))
+    t.start()
 
-    args = []
-
-    shell = False
-    if os.name == 'nt':
-      shell = True
-      args.append('start')
-      args.append('/B')
-
-    if settings.get('php_path'):
-      args.append(settings.get('php_path'))
-    elif os.name == 'nt':
-      args.append('php')
-
-    args.append(settings.get('phpcbf_path', 'phpcbf'))
-
-    if settings.get('phpcs_standard'):
-      args.append('--standard=' + settings.get('phpcs_standard'))
-
-    if settings.get('additional_args'):
-      args += settings.get('additional_args')
-
-    proc = subprocess.Popen(args, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    if proc.stdout:
-      newContent = proc.communicate(content)[0]
-
+  def process_phpcbf_results(self, newContent, window, content):
     # Get the diff between content and the new content.
     difftxt = self.runDiff(window, content, newContent)
     if not difftxt:
@@ -93,38 +72,7 @@ class PHP_CodeSniffer:
     return difftxt
 
 
-  def runPhpcs(self, window):
-    # PHPCS cmd args.
-    args = []
-
-    shell = False
-    if os.name == 'nt':
-      shell = True
-      args.append('start')
-      args.append('/B')
-
-    if settings.get('php_path'):
-      args.append(settings.get('php_path'))
-    elif os.name == 'nt':
-      args.append('php')
-
-    args.append(settings.get('phpcs_path', 'phpcs'))
-
-    if settings.get('phpcs_standard'):
-      args.append('--standard=' + settings.get('phpcs_standard'))
-
-    if settings.get('additional_args'):
-      args += settings.get('additional_args')
-
-    args.append('--report=' + sublime.packages_path() + '/PHP_CodeSniffer/STPluginReport.php')
-
-    proc = subprocess.Popen(args, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-
-    content = window.active_view().substr(sublime.Region(0, window.active_view().size()))
-
-    if proc.stdout:
-      data = proc.communicate(content)[0]
-
+  def process_phpcs_results(self, data, window):
     outputView = self.initResultsPanel(window)
     self.showResultsPanel(window)
     self.view_type = 'phpcs'
@@ -164,6 +112,46 @@ class PHP_CodeSniffer:
     window.active_view().add_regions('errors', err_regions, settings.get('error_scope'), '../PHP_CodeSniffer/icons/error', sublime.HIDDEN)
     window.active_view().add_regions('warnings', warn_regions, settings.get('warning_scope'), '../PHP_CodeSniffer/icons/warning', sublime.HIDDEN)
 
+  def get_command_args(self, cmd_type):
+    args = []
+
+    if os.name == 'nt':
+      args.append('start')
+      args.append('/B')
+
+    if settings.get('php_path'):
+      args.append(settings.get('php_path'))
+    elif os.name == 'nt':
+      args.append('php')
+
+    if cmd_type == 'phpcs':
+      args.append(settings.get('phpcs_path', 'phpcs'))
+      args.append('--report=' + sublime.packages_path() + '/PHP_CodeSniffer/STPluginReport.php')
+    else:
+      args.append(settings.get('phpcbf_path', 'phpcbf'))
+
+    if settings.get('phpcs_standard'):
+      args.append('--standard=' + settings.get('phpcs_standard'))
+
+    if settings.get('additional_args'):
+      args += settings.get('additional_args')
+
+    return args
+
+  def run_command(self, args, cmd, content, window):
+    shell = False
+    if os.name == 'nt':
+      shell = True
+
+    proc = subprocess.Popen(args, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+    if proc.stdout:
+      data = proc.communicate(content)[0]
+
+    if cmd == 'phpcs':
+      sublime.set_timeout(lambda: self.process_phpcs_results(data, window), 0)
+    else:
+      sublime.set_timeout(lambda: self.process_phpcbf_results(data, window, content), 0)
 
   def initResultsPanel(self, window):
     if not hasattr(self, 'output_view'):
@@ -201,7 +189,7 @@ class PHP_CodeSniffer:
     line   = self.output_view.substr(region)
 
     if line.find('[ Click here to fix this file ]') != -1:
-      phpcs.runPhpcbf(self.window)
+      self.run(self.window, 'phpcbf')
       return
     else:
       match = re.match(r'[^:0-9]+([0-9]+)\s*:', line)
@@ -249,11 +237,12 @@ phpcs = PHP_CodeSniffer()
 
 class PhpcbfCommand(sublime_plugin.WindowCommand):
   def run(self):
-    phpcs.runPhpcbf(self.window)
+    phpcs.run(self.window, 'phpcbf')
 
 class PhpcsCommand(sublime_plugin.WindowCommand):
   def run(self):
-    phpcs.runPhpcs(self.window)
+    phpcs.run(self.window, 'phpcs')
+
 
 class PhpcsEventListener(sublime_plugin.EventListener):
   def __init__(self):
